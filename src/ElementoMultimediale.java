8fc7c58da3d18bf6e5d8c2d7747b91039df71b1f
John <deejay.gvr@gmail.com>
2024-07-26 14:57:15 +0200

    aggiunte classe astratte

---
+++ src/ElementoMultimediale.java
@@ -0,0 +1,7 @@
+public abstract class ElementoMultimediale {
+    protected String titolo;
+
+    public ElementoMultimediale(String titolo) {
+        this.titolo = titolo;
+    }
+}
---
+++ src/ElementoRiproducibile.java
@@ -0,0 +1,8 @@
+public abstract class ElementoRiproducibile extends ElementoMultimediale implements Riproducibile {
+    protected int durata;
+
+    public ElementoRiproducibile(String titolo, int durata) {
+        super(titolo);
+        this.durata = durata;
+    }
+}
